5872c5f2399a5b9978ba0a49b66bc73b2b695d29
adesinna <aladesaea@yahoo.com>
2023-08-11 21:36:40 +0100

    ebash script for installation

--- 2-EKSCTL/bash.sh
+++ 2-EKSCTL/bash.sh
@@ -16,7 +16,27 @@ aws configure set aws_secret_access_key "$aws_secret_access_key"
 aws configure set default.region us-west-1
 aws configure set default.output_format json
 
-# Query EC2 Information
-aws ec2 describe-vpcs
+# Install kubectl binary
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/arm64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/arm64/kubectl.sha256
+sha256sum -c kubectl.sha256
+openssl sha1 -sha256 kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+kubectl version --short --client
 
-echo "AWS CLI setup and EC2 information query completed."
+# Install eksctl
+ARCH=arm64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+echo "AWS CLI setup, kubectl and eksctl installation completed."
