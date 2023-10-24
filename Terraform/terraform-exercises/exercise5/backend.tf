196f847d1a590c101a8afaebca7d09c2fe3f51a8
adesinna <aladesaea@yahoo.com>
2023-10-24 06:15:33 +0100

    backend for state

--- Terraform/terraform-exercises/exercise5/backend.tf
+++ Terraform/terraform-exercises/exercise5/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-    bucket = "terra-state-dove"
-    key    = "terraform/backend"
-    region = "us-east-2"
+  backend "s3" { # allows you to maintain infrastructure state across multiple states
+    bucket = "terra-state-dove"  # the bucket name that should be created
+    key    = "terraform/backend" # create a folder in the bucket
+    region = "us-east-2" # region of the bucket
   }
 }
