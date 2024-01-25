aba9c8a71703fd1594a086d91d5a39cff0ae7ada
adesinna <aladesaea@yahoo.com>
2024-01-25 16:43:56 +0100

    k8

--- Project/1-Jenkins/main.tf
+++ Project/1-Jenkins/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "jenkins-server" {
-  ami                    = "ami-0cbd40f694b804622"
+  ami                    = "ami-04f5097681773b989"
   instance_type          = "t2.medium"
-  availability_zone      = "us-west-1a"
-  key_name               = "kube-demo"
-  vpc_security_group_ids = ["sg-001b025721b4285f4"]
+  availability_zone      = "ap-southeast-2a"
+  key_name               = "jenkins-key"
+  vpc_security_group_ids = ["sg-092890e1846a041b5"]
 
   tags = {
     Name    = "jenkins-server"
